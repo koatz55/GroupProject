406fea661400783f0c62d0dd88c20b4f7ce43ed1
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-05-23 18:59:19 -0400

    moise v1

---
+++ Tour_Guide/server.py
@@ -0,0 +1,7 @@
+from flask_app import app # this gets imported
+from flask_app.controllers import itinerariesControllers, usersControllers, login_regControllers
+# we also import our controllers to that they may connect to the server.
+
+
+if __name__=="__main__":
+    app.run(debug=True)
